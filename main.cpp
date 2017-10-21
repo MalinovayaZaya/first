9c33b3a2fd435f041f0f173795fb34619173a187
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-10-21 13:00:21 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
-  //Напишите программу проверяющую N чисел на четность. В программе гарантируется, что все числа целые и их не менее одного
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-
-    int a, N;
-    cin >> N;
-    for(int i=0; i<N ; i++)
-    {
-        cin >> a;
-        if(a%2==0)
-        {
-            cout << a << " - EVEN" << endl;
-        }
-        else{cout << a << " - ODD" << endl;
-        }
-    }
+ int a,b,c=0;
+ cin >> a >> b;
+ for(int i=0; i<b;i++)
+ {
+     c=c+a;
+ }
+ cout << c;
     return 0;
 }
