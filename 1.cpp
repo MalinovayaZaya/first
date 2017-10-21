a32327e80919948098ae0ab85eca78eb8e739b26
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2017-10-21 15:14:29 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main() 
 { 
 int a, N; 
-cin » N; 
+cin >> N; 
 for(int i=0; i<N ; i++) 
 { 
-cin » a; 
+cin >> a; 
 if(a%2==0) 
 { 
-cout « a « " - EVEN" « endl; 
+cout << a << " - EVEN" << endl; 
 } 
-else{cout « a « " - ODD" « endl; 
+else{cout<< a << " - ODD" << endl; 
 } 
 } 
 return 0; 
